197daacf2f75870a2ec3a70e13993ba44ce2e8bd
Max Ishchenko <max.ishchenko@gmail.com>
2008-03-17 22:20:01 +0000

    Restoring after IDEA buggy svn plug-in deleted it

---
+++ src/org/intellij/lang/jflex/psi/JFlexPsiFile.java
@@ -0,0 +1,16 @@
+package org.intellij.lang.jflex.psi;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Max
+ * Date: 17.03.2008
+ * Time: 2:18:18
+ */
+public interface JFlexPsiFile extends JFlexElement {
+
+    JFlexElement getClassname();
+
+    JFlexElement getReturnType();
+
+
+}
